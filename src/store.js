42f1ac6e612b8a108949739d2cd3c329f141fd64
gexiaowei <gandxiaowei@gmail.com>
2021-07-05 15:02:26 +0800

    fix wechat open id save key

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 import axios from 'axios'
 import qs from 'qs'
 
-const WECHAT_OPEN_ID_KEY = 'wechat_open_id_fix'
+const WECHAT_OPEN_ID_KEY = 'wechat_open_id_new'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
